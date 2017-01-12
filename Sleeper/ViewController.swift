00aaaac0a8833298ca20ede7ca1e32550d229987
Ezekiel Elin <ezekielelin@me.com>
2017-01-12 11:09:31 -0800

    Finished rewrite

--- Sleeper/ViewController.swift
+++ Sleeper/ViewController.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 import HealthKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var saveButton: UIButton!
-    @IBOutlet weak var segment: UISegmentedControl!
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
     
-    var wentToSleep: Date? = nil
-    var wokeUp: Date? = nil
+    let defaults = UserDefaults.standard
+    
+    @IBOutlet var sleepButton: UIButton!
+    @IBOutlet var wakeButton: UIButton!
+    @IBOutlet var wakeAtButton: UIButton!
+    @IBOutlet var wakeAtSaveButton: UIButton!
+    @IBOutlet var openHealthButton: UIButton!
+    
+    @IBOutlet var sleepLabel: UILabel!
+    
+    @IBOutlet var wakeAtPicker: UIDatePicker!
+    
+    @IBOutlet var saveIndicator: UIActivityIndicatorView!
+    
+    let bedToSleep: TimeInterval = 60*15 /* 15 minutes */
+    let sleepToBed: TimeInterval = 60*5 /* 5 minutes */
     
     var healthStore = HKHealthStore()
     var sharingAuthorized: Bool {
@@ -25,6 +36,12 @@ class ViewController: UIViewController {
         }
     }
     let sleepType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+    
+    var sleepPressedAt: Date? = nil {
+        didSet {
+            defaults.set(sleepPressedAt, forKey: "wentToSleep")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,90 +54,211 @@ class ViewController: UIViewController {
             if error != nil {
                 print(error?.localizedDescription ?? "Error")
             } else {
-                print("Status: \(success)")
+                print("Health Status: \(success ? "Enabled" : "Disabled")")
             }
         }
         
-        segment.selectedSegmentIndex = 0
-        segment.setEnabled(false, forSegmentAt: 1)
-    }
-    
-    @IBAction func onDidChangeDate(sender: UIDatePicker) {
-        switch segment.selectedSegmentIndex {
-        case 0:
-            wentToSleep = datePicker.date
-        case 1:
-            wokeUp = datePicker.date
-        default:
-            break
-        }
-    }
-    
-    @IBAction func segmentChanged(sender: UISegmentedControl) {
-        switch segment.selectedSegmentIndex {
-        case 0:
-            if let date = wentToSleep {
-                datePicker.date = date
-            }
-        case 1:
-            if let date = wokeUp {
-                datePicker.date = date
-            } else if let date = wentToSleep {
-                datePicker.date = date.addingTimeInterval(28800)
-            }
-        default:
-            break
-        }
-
-    }
-
-    @IBAction func makeDateNow(sender: UIButton) {
-        datePicker.date = Date()
-    }
-    
-    @IBAction func saveSleep(sender: UIButton) {
-        switch segment.selectedSegmentIndex {
-        case 0:
-            wentToSleep = datePicker.date
-            segment.setEnabled(true, forSegmentAt: 1)
-            segment.selectedSegmentIndex = 1
-            if let date = wokeUp {
-                datePicker.date = date
-            } else {
-                datePicker.date = datePicker.date.addingTimeInterval(28800)
-                wokeUp = datePicker.date
-            }
-        case 1:
-            wokeUp = datePicker.date
-            segment.selectedSegmentIndex = 0
-            segment.setEnabled(false, forSegmentAt: 1)
-            saveSleepAnalysis()
-        default:
-            break
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func saveSleepAnalysis() {
+        sleepPressedAt = defaults.object(forKey: "wentToSleep") as? Date
         
-        if sharingAuthorized {
-            let inBedValue = HKCategoryValueSleepAnalysis.inBed.rawValue
-            let object = HKCategorySample(type: sleepType, value: inBedValue, start: Date(), end: Date())
+        if let _ = sleepPressedAt {
+            refreshUI(sleeping: true)
+        } else {
+            refreshUI(sleeping: false)
+        }
+    }
+    
+    func save(_ type: HKCategoryValueSleepAnalysis, from startDate: Date, to endDate: Date, completion: @escaping (Bool, Error?) -> Void) {
+        let sample = HKCategorySample(type: sleepType, value: type.rawValue, start: startDate, end: endDate)
+        healthStore.save(sample) { (success, error) in
+            completion(success, error)
+        }
+    }
+    
+    func save(from bedStart: Date, to bedEnd: Date) {
+        DispatchQueue.main.async {
+            self.hideAll()
+            self.saveIndicator.startAnimating()
             
-            healthStore.save(object, withCompletion: { (success, error) in
-                if error != nil {
-                    print(error?.localizedDescription ?? "Error")
-                } else {
-                    print("Write: \(success)")
-                }
-            })
+            let sleepStart = bedStart.addingTimeInterval(self.bedToSleep)
+            let sleepEnd = bedEnd.addingTimeInterval(-self.sleepToBed)
+            
+            if bedEnd > bedStart && sleepEnd > sleepStart {
+                
+                self.save(.asleep, from: sleepStart, to: sleepEnd, completion: { (sleepSuccess, sleepError) in
+                    self.save(.inBed, from: bedStart, to: bedEnd, completion: { (bedSuccess, bedError) in
+                        let alertTitle = "Unable to Save"
+                        var alertString = "An unknown issue occurred"
+                        
+                        if !sleepSuccess || !bedSuccess {
+                            if let error = sleepError, !sleepSuccess {
+                                alertString = "\(error)"
+                            } else {
+                                if let error = bedError, !bedSuccess {
+                                    alertString = "\(error)"
+                                }
+                            }
+                            
+                            self.alertUser(title: alertTitle, body: alertString)
+                        } else {
+                            print("Saved all Health information")
+                        }
+                        
+                        self.sleepPressedAt = nil
+                        self.refreshUI(sleeping: false)
+                    })
+                })
+                
+            } else {
+                
+                self.alertUser(title: "Unable to Save", body: "You cannot sleep for less than 20 minutes")
+                
+                print("Cancelled due to 20 minute timer")
+                
+                self.sleepPressedAt = nil
+                self.refreshUI(sleeping: false)
+            }
+        }
+    }
+    
+    func refreshUI(sleeping: Bool) {
+        sleepButton.isHidden = sleeping
+        wakeButton.isHidden = !sleeping
+        wakeAtButton.isHidden = !sleeping
+        sleepLabel.isHidden = !sleeping
+        
+        openHealthButton.isHidden = false
+        wakeAtPicker.isHidden = true
+        wakeAtSaveButton.isHidden = true
+        
+        saveIndicator.stopAnimating()
+        
+        if let date = sleepPressedAt, sleeping {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .short
+            formatter.timeStyle = .short
+            let formatted = formatter.string(from: date)
+            sleepLabel.text = "Sleeping since \(formatted)"
         }
         
+        let icon = UIApplicationShortcutIcon(type: .time)
+        
+        let sleep = UIApplicationShortcutItem(type: "com.ezekielelin.sleeper.sleep", localizedTitle: "Sleep", localizedSubtitle: nil, icon: icon, userInfo: nil)
+        let wake = UIApplicationShortcutItem(type: "com.ezekielelin.sleeper.wake", localizedTitle: "Wake", localizedSubtitle: nil, icon: icon, userInfo: nil)
+        
+        if sleeping {
+            UIApplication.shared.shortcutItems = [wake]
+        } else {
+            UIApplication.shared.shortcutItems = [sleep]
+        }
     }
-
+    
+    func hideAll() {
+        sleepButton.isHidden = true
+        wakeButton.isHidden = true
+        wakeAtButton.isHidden = true
+        sleepLabel.isHidden = true
+        
+        saveIndicator.stopAnimating()
+        
+        openHealthButton.isHidden = true
+        wakeAtPicker.isHidden = true
+        wakeAtSaveButton.isHidden = true
+    }
+    
+    @IBAction func sleepButtonPress() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let wakeUp = UNNotificationAction(identifier: "com.ezekielelin.sleeper.notification.wake.action", title: "Wake Up")
+        let category = UNNotificationCategory(identifier: "com.ezekielelin.sleeper.notification.wake", actions: [wakeUp], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+        
+        let now = Date()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                center.removeAllPendingNotificationRequests()
+                center.removeAllDeliveredNotifications()
+                
+                let notif = now.addingTimeInterval(60*30) //Half an hour from now
+                
+                let flags: Set<Calendar.Component> = [.hour, .minute, .second, .year, .month, .day, .calendar]
+                let componenet = Calendar.current.dateComponents(flags, from: notif)
+                
+                
+                let trigger = UNCalendarNotificationTrigger(dateMatching: componenet, repeats: false)
+                
+                let content = UNMutableNotificationContent()
+                content.title = "Wake Up"
+                content.body = "Press to wake up from sleep"
+                content.categoryIdentifier = "com.ezekielelin.sleeper.notification.wake"
+                content.sound = UNNotificationSound.default()
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                center.add(request)
+            } else {
+                self.alertUser(title: "Unable to Schedule Notifications", body: "You must enable notifications in Settings")
+            }
+        }
+        
+        sleepPressedAt = now
+        refreshUI(sleeping: true)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case UNNotificationDefaultActionIdentifier:
+            wakeButtonPress()
+        case "com.ezekielelin.sleeper.notification.wake.action":
+            wakeButtonPress()
+        default:
+            print("Unknown action")
+        }
+        
+        completionHandler()
+    }
+    
+    @IBAction func wakeButtonPress() {
+        if let bedStart = sleepPressedAt {
+            save(from: bedStart, to: Date())
+        } else {
+            sleepPressedAt = nil
+            refreshUI(sleeping: false)
+        }
+    }
+    
+    @IBAction func wakeAtButtonPress() {
+        hideAll()
+        
+        wakeAtPicker.date = sleepPressedAt?.addingTimeInterval(8*60*60) ?? Date()
+        
+        wakeAtPicker.isHidden = false
+        wakeAtSaveButton.isHidden = false
+    }
+    
+    @IBAction func wakeAtSaveButtonPressed() {
+        let bedEnd = wakeAtPicker.date
+        
+        if let bedStart = sleepPressedAt {
+            save(from: bedStart, to: bedEnd)
+        } else {
+            sleepPressedAt = nil
+            refreshUI(sleeping: false)
+        }
+    }
+    
+    @IBAction func openHealthApp() {
+        UIApplication.shared.open(URL(string: "x-apple-health://")!)
+    }
+    
+    func alertUser(title: String, body: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: body, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
